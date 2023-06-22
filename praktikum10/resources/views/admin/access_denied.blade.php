232a4f2686904580af98e5c133427d1baecb65c0
AlifNM0323 <betchesanm14@gmail.com>
2023-06-22 11:29:06 +0700

    Praktikum13

---
+++ praktikum10/resources/views/admin/access_denied.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.layout.appadmin')
+@section ('content')
+<h1>Kesian Amat ditolak Aksesnya wkwk!!!!</h1>
+@endsection
--- praktikum10/routes/web.php
+++ praktikum10/routes/web.php
@@ -5,6 +5,8 @@
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\TampilanController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,23 +20,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 
 
 // Route::get('/dashboard/ad', function () {
 //     return view('front.oke');
 // });
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::get('/produk', [ProdukController::class, 'index']);
-Route::get('/produk/create', [ProdukController::class, 'create']);
-Route::post('/produk/store', [ProdukController::class, 'store']);
-Route::get('produk/edit/{id}', [ProdukController::class, 'edit']);
-Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
-Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+Route::get('/',[TampilanController::class,'index'])->name('public.index');
+Auth::routes();
 
-Route::get('/home', [FrontendController::class, 'index1']);
-Route::get('/about', [FrontendController::class, 'index2']);
+Route::group(['middleware' => ['auth']], function () {
+    // bikin rote baru gup
+    Route::group(['middleware' => ['auth', 'peran:admin-manager']], function () {
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+        Route::get('/produk', [ProdukController::class, 'index']);
+        Route::get('/produk/create', [ProdukController::class, 'create']);
+        Route::post('/produk/store', [ProdukController::class, 'store']);
+        Route::get('produk/edit/{id}', [ProdukController::class, 'edit']);
+        Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
+        Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+    });
 
+    Route::get('/home', [FrontendController::class, 'index1']);
+    Route::get('/about', [FrontendController::class, 'index2']);
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/after_register', function () {
+        return view('after_register');
+    });
+});
+
+
+Route::get('/', [TampilanController::class, 'index']);
